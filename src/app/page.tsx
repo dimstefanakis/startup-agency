22341bba9e1d195b30c43c9a2f9686f0e651a6cd
Dimitris <jimstef@outlook.com>
2023-05-18 03:19:00 +0300

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,87 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+// import Image from "next/image";
+import {
+  Container,
+  Grid,
+  Card,
+  Row,
+  Col,
+  Text,
+  Image,
+} from "@nextui-org/react";
+import NavigationBar from "@/flat/NavigationBar";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <Container
+      css={{ display: "flex", flexFlow: "column", minHeight: "100vh" }}
+    >
+      <NavigationBar />
+      <Container
+        fluid
+        css={{
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          justifyContent: "center",
+          marginTop: "4rem",
+        }}
+      >
+        <Container
+          fluid
+          css={{
+            display: "flex",
+            flexFlow: "column",
+            width: "40%",
+            height: "100%",
+            margin: 0,
+          }}
+        >
+          <Text
+            size="$7xl"
+            css={{
+              lineHeight: "1",
+            }}
+            h1
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
+            Premium Startup Development Agency
+          </Text>
+          <Text
+            size="$lg"
+            h2
+            css={{
+              marginTop: "2rem",
+              color: "$gray800",
+            }}
+          >
+            Transforming Ideas into Successful Ventures
+          </Text>
+        </Container>
+        <Container
+          fluid
+          css={{
+            display: "flex",
+            flexFlow: "column",
+            width: "60%",
+            height: "100%",
+            margin: 0,
+            position: "relative",
+          }}
         >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+          <Image
+            alt=""
+            src="/agency-image.png"
+            width={"100%"}
+            height={500}
+            css={{
+              position: "absolute",
+              right: "0",
+              // bottom: "0",
+            }}
+          />
+        </Container>
+      </Container>
+    </Container>
+  );
 }
---
+++ src/flat/NavigationBar/index.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Navbar, Button, Link, Text, Card, Radio } from "@nextui-org/react";
+// import { AcmeLogo } from "./AcmeLogo.js";
+
+export default function NavigationBar() {
+  return (
+    <Navbar isBordered variant="floating">
+      <Navbar.Brand>
+        {/* <AcmeLogo /> */}
+        <Text b color="inherit" hideIn="xs">
+          SENEC
+        </Text>
+      </Navbar.Brand>
+      <Navbar.Content hideIn="xs">
+        <Navbar.Link href="#">Features</Navbar.Link>
+        {/* <Navbar.Link isActive href="#">
+          Customers
+        </Navbar.Link> */}
+        {/* <Navbar.Link href="#">Pricing</Navbar.Link> */}
+        <Navbar.Link href="#">Company</Navbar.Link>
+      </Navbar.Content>
+      <Navbar.Content>
+        {/* <Navbar.Link color="inherit" href="#">
+          Login
+        </Navbar.Link> */}
+        <Navbar.Item>
+          <Button auto flat as={Link} href="#">
+            Book A Call
+          </Button>
+        </Navbar.Item>
+      </Navbar.Content>
+    </Navbar>
+  );
+}
