809bb683a8ea6f60abe9002323929d39aebd903e
Dimitris <jimstef@outlook.com>
2023-05-18 05:29:54 +0300

    added development section

--- src/flat/OurServices/index.tsx
+++ src/flat/OurServices/index.tsx
@@ -2,6 +2,15 @@ import { Container, Text, Image } from "@nextui-org/react";
 
 function OurServices() {
   return (
+    <>
+      <Payments />
+      <Development />
+    </>
+  );
+}
+
+function Payments() {
+  return (
     <Container
       fluid
       css={{
@@ -92,6 +101,135 @@ function OurServices() {
             margin: 0,
           }}
         />
+      </Container>
+    </Container>
+  );
+}
+
+function Development() {
+  return (
+    <Container
+      fluid
+      css={{
+        display: "flex",
+        flexFlow: "column",
+        width: "100%",
+        height: "100%",
+        margin: 0,
+      }}
+    >
+      <Text
+        size="$5xl"
+        css={{
+          lineHeight: "1",
+        }}
+        h1
+      >
+        Development
+      </Text>
+      <Container css={{ padding: 0, display: "flex" }}>
+        <Image
+          src="/development.svg"
+          alt="card"
+          width={"30%"}
+          height={500}
+          css={{
+            filter: "brightness(0) invert(1)",
+            margin: 0,
+          }}
+        />
+
+        <Container
+          css={{
+            width: "70%",
+            padding: 0,
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <Text
+            size="$xl"
+            h2
+            css={{
+              marginTop: "2rem",
+              color: "$gray800",
+              maxWidth: "70%",
+            }}
+          >
+            Building robust, scalable solutions is our forte at Senec.
+            Specializing in Software as a Service (SaaS) and hybrid
+            applications, our development services cater to startups looking for
+            efficiency, flexibility, and reach.
+            <br />
+            <br />
+            We craft tailor-made solutions that support your unique business
+            needs and growth trajectory. Our experienced developers utilize
+            cutting-edge technologies and methodologies to deliver high-quality
+            products that stand the test of time and scale.
+          </Text>
+          <Container css={{ padding: 0, display: "flex", maxWidth: "70%" }}>
+            <img
+              src="/logos/python.png"
+              alt="python"
+              style={{
+                width: 50,
+                height: 50,
+                margin: 0,
+                marginRight: 10,
+              }}
+            />
+            <img
+              src="/logos/react.png"
+              alt="react"
+              style={{
+                width: 50,
+                height: 50,
+                margin: 0,
+                marginRight: 10,
+              }}
+            />
+            <img
+              src="/logos/docker.png"
+              alt="docker"
+              style={{
+                width: 50,
+                height: 50,
+                margin: 0,
+                marginRight: 10,
+              }}
+            />
+            <img
+              src="/logos/supabase.png"
+              alt="supabase"
+              style={{
+                width: 50,
+                height: 50,
+                margin: 0,
+                marginRight: 10,
+              }}
+            />
+            <img
+              src="/logos/amazon.png"
+              alt="amazon"
+              style={{
+                width: 50,
+                height: 50,
+                margin: 0,
+                marginRight: 10,
+              }}
+            />
+            <img
+              src="/logos/mailchimp.png"
+              alt="mailchimp"
+              style={{
+                width: 50,
+                height: 50,
+                margin: 0,
+                marginRight: 10,
+              }}
+            />
+          </Container>
+        </Container>
       </Container>
     </Container>
   );
